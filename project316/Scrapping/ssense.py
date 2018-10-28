e7235a76b5aec6f01adee71b7bf74fb3e45378b8
evanWang1409 <evnw1409@hotmail.com>
2018-10-27 22:24:58 -0400

    update

--- project316/Scrapping/ssense.py
+++ project316/Scrapping/ssense.py
@@ -1,15 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import get_img_url
-import get_product_description
-import get_product_url
+from. import get_img_url
+from. import get_product_description
+from. import get_product_url
+import argparse
 
-men_category = ('men/jackets-coats', 'men/jeans', 'men/pants',
-				'men/shirts', 'men/shorts', 'men/suits-blazers',
-				'men/sweaters')
-women_category = ('women/dresses', 'women/jackets-coats', 'women/jeans', 'women/jumpsuits',
-				'women/pants', 'women/shorts', 'women/skirts', 'women/sweaters', 'women/tops')
+men_category = ('jackets-coats', 'jeans', 'pants',
+				'shirts', 'shorts', 'suits-blazers',
+				'sweaters')
+women_category = ('dresses', 'jackets-coats', 'jeans', 'jumpsuits',
+				'pants', 'shorts', 'skirts', 'sweaters', 'tops')
 
 root = 'https://www.ssense.com/en-us/'
 
@@ -17,7 +18,7 @@ def scrap():
 	clothes = []
 	index = 0
 	for cat in men_category:
-		cat_url = root+cat
+		cat_url = root + 'men/' + cat
 		product_urls = get_product_url(cat_url)
 		for product_url in product_urls:
 			index += 1
@@ -27,12 +28,13 @@ def scrap():
 			temp.append(index)
 			temp.append(product_description[0])
 			temp.append('men')
+			temp.append(cat)
 			temp.append(img_url)
 			temp.append(product_description[1])
 			clothes.append(temp)
 
 	for cat in women_category:
-		cat_url = root+cat
+		cat_url = root + 'women/' + cat
 		product_urls = get_product_url(cat_url)
 		for product_url in product_urls:
 			index += 1
@@ -42,11 +44,40 @@ def scrap():
 			temp.append(index)
 			temp.append(product_description[0])
 			temp.append('women')
+			temp.append(cat)
 			temp.append(img_url)
 			temp.append(product_description[1])
 			clothes.append(temp)
 
+	return clothes
+
+def arg():
+	parser = argparse.ArgumentParser(description='save data directory')
+	parser.add_argument('--csv_dir', dest = csv_dir, type=str,
+                    default='/Users/evnw/Documents/GitHub/FloorDog/project316')
+
+def save_csv(args, clothes):
+	csv_path = args.csv_dir + '/' + "clothes.csv"
+	with open(csv_path, 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile, delimiter=',')
+		for cloth in clothes:
+			writer.write(cloth)
 
 
 if __name__ == '__main__':
-	scrap()
+	args = arg()
+	clothes = scrap()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
