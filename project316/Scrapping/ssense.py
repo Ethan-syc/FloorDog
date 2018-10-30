ff6617b0a58aab7b9696c870d09a3ed722665f3c
evanWang1409 <evnw1409@hotmail.com>
2018-10-30 19:59:24 -0400

    improved scrapping
    use try-except to deal with occational error
    added flipping page function
    now able to access all products within any catagory

--- project316/Scrapping/ssense.py
+++ project316/Scrapping/ssense.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 from scrapping_tools import scrapping_tools
 import argparse
-import csv
+import csv, os
 
 men_category = ('jackets-coats', 'jeans', 'pants',
 				'shirts', 'shorts', 'suits-blazers',
@@ -16,7 +16,7 @@ root = 'https://www.ssense.com'
 catagory_max = 1000
 
 def scrap(args):
-	csv_path = args.csv_dir + '/' + "clothes.csv"
+	csv_path = os.path.join(args.csv_dir, "clothes.csv")
 	with open(csv_path, 'w', newline='') as csvfile:
 		writer = csv.writer(csvfile, delimiter=',')
 
@@ -29,32 +29,52 @@ def scrap(args):
 			product_index = 0
 			print("men catagory: {}/{}".format(catagory_index, len(men_category)))
 
-			cat_url = root + '/en-us/' + 'men/' + cat
-			product_urls = scrapping_tools.get_product_url(cat_url)
+			for i in range(1, 100):
 
-			for product_url in product_urls:
-				product_index += 1
-				index += 1
+				cat_url = root + '/en-us/' + 'men/' + cat
+				if i != 1:
+					cat_url += "?page={}".format(i)
+
+				try:
+					product_urls = scrapping_tools.get_product_url(cat_url)
+				except:
+					print("end of catagory: page{}".format(i+1))
+					break
+
+				for product_url in product_urls:
+					product_index += 1
+					index += 1
+
+					if(product_index > 1000):
+						break
+					if(product_index%10 == 0):
+						print("men/{}: page{}, {}/{}".format(cat, i, product_index, len(product_urls)*i))
+
+					product_url = root + product_url
+
+					temp = []
+					try:
+						img_url = scrapping_tools.get_img_url(product_url)
+					except:
+						print("cannot find image url: index-{}, product_url: {}".format(index, product_url))
+						img_url = 'null'
+					try:
+						product_description = scrapping_tools.get_product_description(product_url)
+					except:
+						print("cannot find description: index-{}, product_url: {}".format(index, product_url))
+						product_description=['null','null']
+
+					temp.append(index)
+					temp.append(product_url)
+					temp.append(product_description[0])
+					temp.append('men')
+					temp.append(cat)
+					temp.append(img_url)
+					temp.append(product_description[1])
+					writer.writerow(temp)
 
 				if(product_index > 1000):
-					break
-				if(product_index%10 == 0):
-					print("men/{}: {}/1000".format(cat, product_index))
-
-				product_url = root + product_url
-
-				temp = []
-
-				img_url = scrapping_tools.get_img_url(product_url)
-				product_description = scrapping_tools.get_product_description(product_url)
-
-				temp.append(index)
-				temp.append(product_description[0])
-				temp.append('men')
-				temp.append(cat)
-				temp.append(img_url)
-				temp.append(product_description[1])
-				writer.writerow(temp)
+						break
 
 		catagory_index = 0
 
