e51cfa123286b51bcd370b19378d0af2fd5dcd15
evanWang1409 <evnw1409@hotmail.com>
2018-10-30 20:08:11 -0400

    added same modification for women clothes

--- project316/Scrapping/ssense.py
+++ project316/Scrapping/ssense.py
@@ -24,7 +24,6 @@ def scrap(args):
 		catagory_index = 0
 		
 		for cat in men_category:
-
 			catagory_index += 1
 			product_index = 0
 			print("men catagory: {}/{}".format(catagory_index, len(men_category)))
@@ -84,31 +83,52 @@ def scrap(args):
 			product_index = 0
 			print("women catagory: {}/{}".format(catagory_index, len(women_category)))
 
-			cat_url = root + '/en-us/' + 'women/' + cat
-			product_urls = scrapping_tools.get_product_url(cat_url)
+			for i in range(1, 100):
 
-			for product_url in product_urls:
-				product_index += 1
-				index += 1
+				cat_url = root + '/en-us/' + 'women/' + cat
+
+				if i != 1:
+					cat_url += "?page={}".format(i)
+
+				try:
+					product_urls = scrapping_tools.get_product_url(cat_url)
+				except:
+					print("end of catagory: page{}".format(i+1))
+					break
+
+				for product_url in product_urls:
+					product_index += 1
+					index += 1
+
+					if(product_index > 1000):
+						break
+					if(product_index%10 == 0):
+						print("women/{}: page{}, {}/{}".format(cat, i, product_index, len(product_urls)*i))
+
+					product_url = root + product_url
+					temp = []
+
+					try:
+							img_url = scrapping_tools.get_img_url(product_url)
+					except:
+						print("cannot find image url: index-{}, product_url: {}".format(index, product_url))
+						img_url = 'null'
+					try:
+						product_description = scrapping_tools.get_product_description(product_url)
+					except:
+						print("cannot find description: index-{}, product_url: {}".format(index, product_url))
+						product_description=['null','null']
+
+					temp.append(index)
+					temp.append(product_description[0])
+					temp.append('women')
+					temp.append(cat)
+					temp.append(img_url)
+					temp.append(product_description[1])
+					writer.writerow(temp)
 
 				if(product_index > 1000):
-					break
-				if(product_index%10 == 0):
-					print("men/{}: {}/1000".format(cat, product_index))
-
-				product_url = root + product_url
-				temp = []
-
-				img_url = scrapping_tools.get_img_url(product_url)
-				product_description = scrapping_tools.get_product_description(product_url)
-
-				temp.append(index)
-				temp.append(product_description[0])
-				temp.append('women')
-				temp.append(cat)
-				temp.append(img_url)
-				temp.append(product_description[1])
-				writer.writerow(temp)
+						break
 
 
 def arg():
