efcca36a5ffd8c20323cd22cf4767289c6237c38
Yichuan <yichuan.shi@duke.edu>
2018-11-26 04:50:58 -0500

    add a tempalte html for example

--- website/views.py
+++ website/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse(
-        "Hello, world. This our first page, we really need to hurry up"
-    )
+    return render(request, 'index.html')
